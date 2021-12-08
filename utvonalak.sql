402b3b6559947e1f44b6aa7e42c2e264a4a0fadb
adam026 <76121613+adam026@users.noreply.github.com>
2021-12-08 19:04:00 +0100

    Add files via upload

---
+++ utvonalak.sql
@@ -0,0 +1,28 @@
+﻿--Kezdés 16:35
+--Vége 16:50
+
+--CREATE DATABASE tura;
+--USE tura;
+
+--CREATE TABLE turazo(
+--id INT PRIMARY KEY IDENTITY,
+--vezeteknev VARCHAR(30),
+--keresztnev VARCHAR(30),
+--varos VARCHAR(50),
+--telefonszam VARCHAR(11));
+
+--CREATE TABLE utvonal(
+--id INT PRIMARY KEY IDENTITY,
+--turazoID INT FOREIGN KEY REFERENCES turazo(id),
+--honnan VARCHAR(50),
+--hova VARCHAR (50),
+--km INT);
+
+--INSERT INTO turazo VALUES
+--('Kiss','Manyi','Velence','06701234567'),
+--('Nagy','Imre','Budapest','06703216547'),
+--('Tompa','Mihály','Budapest','06207854563');
+
+--INSERT INTO utvonal VALUES
+--(2,'Budapest','Monor',38),
+--(3,'Budapest','Esztergom',54);
